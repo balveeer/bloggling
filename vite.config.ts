79e098deba85b9756860b4a1f8c0c8c81640acfb
Balveer Singh <bs902423@gmail.com>
2024-08-02 18:38:40 +0530

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "favicon.ico",
-        "apple-touch-icon.png",
-        "safari-pinned-tab.svg",
-        "mstile-150x150.png",
-      ],
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,jpg,png,svg,webp,aviv}'],
+      },
       manifest: {
-        name: "Bloggling",
-        short_name: "Bloggling",
-        description: "Mind Boggling Blogs...",
-        theme_color: "#ffffff",
+        name: 'Slik',
+        short_name: 'Slik',
+        description: "India's finest fashion hand-picked from top brands",
+        theme_color: '#F3E8FF',
+        background_color: '#F3E8FF',
+        start_url: '/',
+        display: 'standalone',
+        prefer_related_applications: false,
         icons: [
           {
-            src: "favicon-16x16.png",
-            sizes: "16x16",
-            type: "image/png",
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
           },
           {
-            src: "favicon-32x32.png",
-            sizes: "32x32",
-            type: "image/png",
-          },
-          {
-            src: "apple-touch-icon.png",
-            sizes: "180x180",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           },
         ],
+      },
+      devOptions: {
+        enabled: false, // for enabling pwa in dev mode ;)
       },
     }),
   ],
